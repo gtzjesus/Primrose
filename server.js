f839be15ac0ae9aa6eff9719da00488672ca1c5b
gtz.jesus <gtz.jesus@outlook.com>
2023-07-10 16:02:08 -0600

    hello

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', (err) => {
+process.on('uncaughtExceptiheoron', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
